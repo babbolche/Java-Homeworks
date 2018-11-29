3946a5f9ac6c5512d4425f52ebc9282cc59234af
Marina Radeva <marina_radeva@yahoo.com>
2018-11-29 20:24:45 +0200

    Add scanner

--- src/HomeworkOne/Number5.java
+++ src/HomeworkOne/Number5.java
@@ -1,10 +1,16 @@
 package HomeworkOne;
 
+import java.util.Scanner;
+
 public class Number5 {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 1;
-        int c = 8;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick whole number:");
+        int a = scanner.nextInt();
+        System.out.println("Pick one more whole number:");
+        int b = scanner.nextInt();
+        System.out.println("And one more whole number:");
+        int c = scanner.nextInt();
         if (a > b && a > c) {
             if (b > c) {
                 System.out.println(a + " " + b + " " + c);
@@ -19,13 +25,15 @@ public class Number5 {
             } else {
                 System.out.println(b + " " + c + " " + a);
             }
-            if (c > a && c > b) {
-                if (b > a) {
-                    System.out.println(c + " " + b + " " + a);
-                } else {
-                    System.out.println(c + " " + a + " " + b);
-                }
+        }
+        if (c > a && c > b) {
+            if (b > a) {
+                System.out.println(c + " " + b + " " + a);
+            } else {
+                System.out.println(c + " " + a + " " + b);
             }
         }
     }
 }
+
+
