f10e8656af3fd5dffd0d5f265d105c331b27842e
Marina Radeva <marina_radeva@yahoo.com>
2018-12-01 23:14:30 +0200

    Initial commit

---
+++ src/HomeworkTwo/AllNumbers.java
@@ -0,0 +1,32 @@
+//Да се прочетат 2 числа от клавиатурата А и В. Да се изведат всички числа от А до В на степен
+// 2(разделени с запетая). Ако някое число е кратно на 3, да се изведе съобщение че числото се пропуска „skip 3“.
+// Ако сумата от всички изведени числа (без пропуснатите) стане по-голяма от 200, да се прекрати извеждането.
+
+package HomeworkTwo;
+
+import java.util.Scanner;
+
+public class AllNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a = sc.nextInt();
+        System.out.println("Type another number:");
+        int b = sc.nextInt();
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 3 == 0) {
+                System.out.print(" skip 3, ");
+            } else {
+                System.out.print(i * i);
+                sum += i * i;
+                if (sum > 200) {
+                    break;
+                } else {
+                    System.out.print(", ");
+                }
+            }
+        }
+    }
+}
+
---
+++ src/HomeworkTwo/SimpleNumber.java
@@ -0,0 +1,15 @@
+//Въведете число от клавиатурата и определете дали е просто.
+// Просто число е това което се дели САМО на 1 и на себе си.
+
+package HomeworkTwo;
+
+import java.util.Scanner;
+
+public class SimpleNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a=sc.nextInt();
+        
+    }
+}
